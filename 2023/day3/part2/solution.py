b005fe08dcd7fccaf87434475232dd50bed44a67
hockeyfriend <48014457+hockeyfriend@users.noreply.github.com>
2026-08-16 12:48:32 +0000

    refactored code into class

--- 2023/day3/part2/solution.py
+++ 2023/day3/part2/solution.py
@@ -1,77 +1,127 @@
 import os
 
-def checkPos(lines, x, y):
-    if x < 0 or y < 0 or y >= len(lines) or x >= len(lines[0]):
-        return None
 
-    char = lines[y][x]
-    if char.isdigit():
-        return int(char)
-    else:
-        return None
+class GearRatioCalculator:
+    def __init__(self, lines):
+        self.lines = lines
+        self.gearSymbol = "*"
 
-def getGearNumbers(lines, xIdx, yIdx):
-    idxHits = set()
-    gearNumbers = []
+    def checkPos(self, x, y):
+        """Check if position is valid and contains a digit.
+        
+        Returns:
+            int or None: The digit at the position, or None if invalid.
+        """
+        if x < 0 or y < 0 or y >= len(self.lines) or x >= len(self.lines[0]):
+            return None
 
-    for i in [1, 0, -1]:
-        for k in [1, 0, -1]:
-            if i == 0 and k == 0:
-                continue
+        char = self.lines[y][x]
+        if char.isdigit():
+            return char
+        else:
+            return None
 
-            x = xIdx + k
-            y = yIdx + i
+    def getGearNumbers(self, x, y):
+        """Get all numbers adjacent to a gear at position (x, y).
+        
+        Returns:
+            list: List of numbers adjacent to the gear.
+        """
+        idxHits = set()
+        gearNumbers = []
 
-            if (x,y) in idxHits:
-                continue
+        # Check all 8 surrounding positions
+        for i in [1, 0, -1]:
+            for k in [1, 0, -1]:
+                if i == 0 and k == 0:
+                    continue
 
-            if checkPos(lines, x, y) is not None:
-                idxHits.add((x, y))
-                centralDigit = lines[y][x]
-                
-                gearNumber = ""   
-                xForward = x
+                posX = x + k
+                posY = y + i
 
-                while True:
-                    xForward -= 1
-                    if(xForward == -1):
-                        break
+                if (posX, posY) in idxHits:
+                    continue
 
-                    if lines[y][xForward].isdigit():
-                        idxHits.add((xForward,y))
-                        gearNumber = lines[y][xForward] + gearNumber
-                    else:
-                        break
-                
-                gearNumber += centralDigit
+                centralDigit = self.checkPos(posX, posY)
+                if centralDigit is not None:
+                    idxHits.add((posX, posY))
 
-                xBackward = x
+                    # Build complete number by scanning left
+                    gearNumber = ""
+                    xForward = posX
 
-                while True:
-                    xBackward += 1
-                    if(xBackward == len(lines[y])):
-                        break
+                    while True:
+                        xForward -= 1
+                        if xForward == -1:
+                            break
 
-                    if lines[y][xBackward].isdigit():
-                        idxHits.add((xBackward, y))
-                        gearNumber += lines[y][xBackward]
-                    else:
-                        break
-                
-                gearNumbers.append(int(gearNumber))
+                        if self.lines[posY][xForward].isdigit():
+                            idxHits.add((xForward, posY))
+                            gearNumber = self.lines[posY][xForward] + gearNumber
+                        else:
+                            break
 
-    return gearNumbers
+                    gearNumber += centralDigit
 
-def calcGearRatio(lines, x, y):
-    gearNumbers = getGearNumbers(lines, x,y)
+                    # Build complete number by scanning right
+                    xBackward = posX
 
-    if len(gearNumbers) <= 1:
-        return 0
-    
-    prod = 1
-    for num in gearNumbers:
-        prod *= num
-    return prod
+                    while True:
+                        xBackward += 1
+                        if xBackward == len(self.lines[posY]):
+                            break
+
+                        if self.lines[posY][xBackward].isdigit():
+                            idxHits.add((xBackward, posY))
+                            gearNumber += self.lines[posY][xBackward]
+                        else:
+                            break
+
+                    gearNumbers.append(int(gearNumber))
+
+        return gearNumbers
+
+    def calcGearRatio(self, x, y):
+        """Calculate gear ratio for a gear at position (x, y).
+        
+        Returns:
+            int: The product of the two adjacent numbers, or 0 if not exactly 2 numbers.
+        """
+        gearNumbers = self.getGearNumbers(x, y)
+
+        if len(gearNumbers) != 2:
+            return 0
+
+        prod = 1
+        for num in gearNumbers:
+            prod *= num
+        return prod
+
+    def solve(self):
+        """Solve the puzzle by calculating sum of all gear ratios.
+        
+        Returns:
+            int: Sum of all gear ratios.
+        """
+        total = 0
+        lineIndex = 0
+
+        for line in self.lines:
+            searchIdx = 0
+
+            while True:
+                gearIdx = line.find(self.gearSymbol, searchIdx)
+
+                if gearIdx >= 0:
+                    searchIdx = gearIdx + 1
+                    gearRatio = self.calcGearRatio(gearIdx, lineIndex)
+                    total += gearRatio
+                else:
+                    break
+            lineIndex += 1
+
+        return total
+
 
 def main():
     p = os.path.join(os.path.dirname(__file__), "puzzle.txt")
@@ -79,24 +129,10 @@ def main():
     with open(p, "r", encoding="utf-8") as file:
         lines = file.read().splitlines()
 
-    symbol = "*"
-    sum = 0
-    lineIndex = 0
+    calculator = GearRatioCalculator(lines)
+    result = calculator.solve()
+    print(result)
 
-    for line in lines:
-        searchIdx = 0
-
-        while True:
-            fstSymbolIdx = line.find(symbol, searchIdx)
-            
-            if fstSymbolIdx >= 0:
-                searchIdx = fstSymbolIdx + 1
-                gearRatio = calcGearRatio(lines, fstSymbolIdx, lineIndex)
-                sum += gearRatio
-            else:
-                break
-        lineIndex += 1
-    print(sum)
 
 if __name__ == "__main__":
     main()
